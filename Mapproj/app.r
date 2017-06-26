67071842803b64c1a452008fd56638db33ee0b69
maxpohlman <maxpohlman@gmail.com>
2017-06-26 02:07:34 -0400

    try

--- Mapproj/app.r
+++ Mapproj/app.r
@@ -5,15 +5,15 @@ library(ggplot2)
 library(dplyr)
 options(shiny.sanitize.errors = FALSE)
 
-streams <- st_read("ridata/streams.shp")
+#streams <- st_read("ridata/streams.shp")
 muni <- st_read("ridata/muni97d.shp")
 muni <- st_transform(muni, 4326)
-streams <- st_transform(streams, 4326)
-lu <- st_read("ridata/Land_Use_2025.shp")
-road<- st_read('ridata/RIDOT_Roads_2016.shp')
-busroutes <- st_read('ridata/RIPTA_Bus_Routes.shp')
-pond <- st_read('ridata/Rhode_Island_Ponds_and_Lakes.shp')
-census <- st_read('ridata/US_Census_2010_Summary_File_1_Indicators.shp')
+#streams <- st_transform(streams, 4326)
+#lu <- st_read("ridata/Land_Use_2025.shp")
+#road<- st_read('ridata/RIDOT_Roads_2016.shp')
+#busroutes <- st_read('ridata/RIPTA_Bus_Routes.shp')
+#pond <- st_read('ridata/Rhode_Island_Ponds_and_Lakes.shp')
+#census <- st_read('ridata/US_Census_2010_Summary_File_1_Indicators.shp')
 
 shinyApp( 
   
